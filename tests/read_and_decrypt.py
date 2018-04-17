1241ae6fb0f270a0ea01594a4c461cec099f9700
Bradley Gannon <bradley.m.gannon@gmail.com>
2018-04-17 00:25:11 +0000

    Add test for read_and_decrypt()

--- tests/read_and_decrypt.py
+++ tests/read_and_decrypt.py
@@ -1,6 +1,13 @@
+import os
 import unittest
 import passport
 
 class ReadAndDecrypt(unittest.TestCase):
-    pass
-    # TODO add some tests
+    def test_empty_database(self):
+        test_file = open("testdb","wb")
+        test_file.write(b"\x85[\xd4T")
+        test_file.close()
+        self.assertEqual(passport.read_and_decrypt("testdb","blergh"),
+                ({},"blergh"))
+        os.system("rm testdb")
+    # TODO add more tests
