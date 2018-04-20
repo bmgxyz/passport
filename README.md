708ac0805445de7a414c698fd9398aa336fa4d05
Bradley Gannon <bradley.m.gannon@gmail.com>
2018-04-20 16:28:44 +0000

    Add basic logging and update README

--- README.md
+++ README.md
@@ -5,33 +5,45 @@ a command-line password manager written in Python
 
 **Write a new empty database file:**
 
-TODO Update this to reflect new features
-
 ```passport <database_name> create```
 
-You will be prompted to set a password for the database. Be sure to use a strong password, and don't forget it. Your data cannot be recovered without the password.
+You will be prompted to set a password for the database. Be sure to use a strong
+password, and don't forget it. Your data cannot be recovered without the
+password.
 
 **Add a new account entry:**
 
 ```passport <database_name> edit <account_name>```
 
-This will launch ```vi```. Enter all the information you want to keep under that account (passwords, 2FA backup codes, last update times, etc.) and save.
+Launches ```vi``` in a ```screen``` session. Enter all the information you want
+to keep under that account (passwords, 2FA backup codes, last update times,
+etc.) and save. Note that the screen session will lock after 30 seconds of
+inactivity, and you'll have to enter your user account password to get back in.
 
 **Display an existing account entry:**
 
 ```passport <database_name> display <account_name>```
 
-This will launch ```less``` and display the information under that account. Press ```q``` to quit.
+This will launch ```less``` and display the information under that account.
+Press ```q``` to quit.
 
 **List the names of all of the entries on the account:**
 
 ```passport <database_name> list```
 
-Note that for security reasons this will only write *account names* to ```stdout```, not any secret data pertaining to them.
+Note that for security reasons this will only write *account names* to
+```stdout```, not any secret data pertaining to them.
 
 **Remove an account from the database:**
 
 ```passport <database_name> remove <account_name>```
+
+**Search the database:**
+
+```passport <database_name> search <query>```
+
+Returns a list of accounts in the database whose entries or titles match
+```<query>```. Note that ```<query>``` can either be plaintext or a regex.
 
 For more information, run ```passport -h```.
 
@@ -39,13 +51,22 @@ For more information, run ```passport -h```.
 
 Full installation instructions TODO
 
+I have no idea how to package Python code for distribution, so you're mostly on
+your own. On my system, I just linked the source code to
+```<python environment>/lib/passport``` and ```passport.py``` to
+```/usr/local/bin/passport``` or ```<python environment>/bin/passport``` to let
+```bash``` pick it up.
+
 **Dependencies**
 
 - pycrypto
 - GNU screen
 
-Note that I've only tested ```passport``` under Python 3.5.2 on Ubuntu 16.04.4. YMMV
+Note that I've only tested ```passport``` under Python 3.5.2 on Ubuntu 16.04.4.
+Also, there are currently
+[zarro boogs](https://en.wikipedia.org/wiki/Bugzilla#Zarro_Boogs). YMMV
 
 ## Contributing
 
-Sure, knock yourself out.
+Sure, knock yourself out. If you know anything about packaging, feel free to
+teach me your ways.
--- src/passport.py
+++ src/passport.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import re
@@ -6,6 +6,7 @@ import sys
 import json
 import random
 import getpass
+import logging
 import argparse
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
@@ -77,7 +78,16 @@ def read_and_decrypt(filename, database_password=False):
         raise Exception("BadKeyError")
 
 if __name__ == "__main__":
+    # set up logging
+    with open(sys.path[-1]+'/passport/logs/latest.log', 'w'):
+        pass
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+            filename=sys.path[-1]+"/passport/logs/latest.log",
+            level=logging.INFO)
+    logging.info("Passport started")
+
     # make top-level parser
+    logging.info("Parsing command line arguments...")
     parser = argparse.ArgumentParser(description="display and manage passwords")
     parser.add_argument("database", type=str, help="the database to work on")
     subparsers = parser.add_subparsers(dest="choice")
@@ -119,33 +129,52 @@ if __name__ == "__main__":
             help="name of the account to remove")
     # parse all arguments and save namespace object
     args = parser.parse_args()
+    logging.info("Arguments parsed successfully")
 
     # if the user doesn't choose a subcommand, then display help and exit
     if args.choice == None:
+        logging.info("No subcommand chosen, exiting")
         parser.print_help(sys.stderr)
         sys.exit(1)
     # otherwise, execute the appropriate subcommand
     elif args.choice == "create":
+        logging.info("User selected 'create' subcommand")
+        logging.info("Creating blank database with name '"+args.database+"'")
         blank_database = json.dumps({})
         encrypt_and_write(blank_database, args.database)
+        logging.info("Blank database written successfully")
     elif args.choice == "list":
+        logging.info("User selected 'list' subcommand")
+        logging.info("Reading and decrypting password database '"+
+                args.database+"'...")
         password_database, database_password = read_and_decrypt(args.database)
+        logging.info("Read and decrypted '"+args.database+"' successfully")
+        logging.info("Printing database entry titles...")
         if password_database == {}:
             print("'"+args.database+"' is empty.")
         else:
             print('\n'.join(sorted(password_database.keys())))
+        logging.info("Done printing, exiting")
     elif args.choice == "edit":
         # read and decrypt password database from disk
+        logging.info("User selected 'edit' subcommand")
+        logging.info("Reading and decrypting password database '"+
+                args.database+"'...")
         password_database, database_password = read_and_decrypt(args.database)
+        logging.info("Read and decrypted '"+args.database+"' successfully")
         # allow the user to update the entry
+        logging.info("Reading existing entry for title '"+args.account+"'...")
         account_name = args.account
         database_name = args.database
         if account_name in password_database.keys():
             existing_entry = password_database[account_name]
         else:
             existing_entry = ""
+        logging.info("Existing entry read in successfully")
         # generate a random password if the user requested one
         if args.generate != None:
+            logging.info("User requested a random "+str(args.generate)+
+                    "-word passphrase. Generating...")
             wordlist_file = open("./src/wordlist.txt","r")
             wordlist = []
             for line in wordlist_file:
@@ -158,10 +187,13 @@ if __name__ == "__main__":
                 existing_entry += "\n\n"
             # append the new password to the entry for editing
             existing_entry += new_password
+            logging.info("Passphrase generated successfully")
         # get entry data from user
+        logging.info("Preparing file at /tmp/passport.tmp for editing...")
         os.system("touch /tmp/passport.tmp")
         os.system("chmod 600 /tmp/passport.tmp")
         os.system("echo '"+existing_entry+"' > /tmp/passport.tmp")
+        logging.info("File prepared, launching vim...")
         # launch vi to edit the entry
         # but lock the screen session after 30 seconds
         os.system("screen -c "+
@@ -169,15 +201,23 @@ if __name__ == "__main__":
         password_database[account_name] = open(
                 "/tmp/passport.tmp","r").read().strip("\n")
         os.system("rm /tmp/passport.tmp")
+        logging.info("Editing complete, temporary file removed")
         # encrypt the database and write it to disk
+        logging.info("Encrypting database and writing to file...")
         encrypt_and_write(password_database,
                 database_name,
                 database_password=database_password)
         output = "Updated information for '"+account_name+"'"
+        logging.info("Wrote encrypted database successfully, exiting")
     elif args.choice == "search":
         # read and decrypt password database
+        logging.info("User selected 'search' subcommand")
+        logging.info("Reading and decrypting password database '"+
+                args.database+"'...")
         password_database, database_password = read_and_decrypt(args.database)
+        logging.info("Read and decrypted '"+args.database+"' successfully")
         # compile the regex
+        logging.info("Searching database...")
         regex = re.compile(args.query)
         # loop over the database and find all matching entries and titles
         matches = []
@@ -185,14 +225,22 @@ if __name__ == "__main__":
             entry = password_database[key]
             if regex.search(key) or regex.search(entry):
                 matches.append(key)
+        logging.info("Search complete, reporting results")
         if matches == []:
             print("No matches in '"+
                     args.database+"' for query '"+args.query+"'")
         else:
             print('\n'.join(matches))
+        logging.info("Results printed, exiting")
     elif args.choice == "display":
+        logging.info("User selected 'display' subcommand")
+        logging.info("Reading and decrypting password database '"+
+                args.database+"'...")
         password_database, database_password = read_and_decrypt(args.database)
+        logging.info("Read and decrypted '"+args.database+"' successfully")
         # confirm that the account exists in the database
+        logging.info("Accessing and displaying entry under '"+
+                args.account+"'...")
         account_name = args.account
         database_name = args.database
         if account_name not in password_database.keys():
@@ -204,38 +252,60 @@ if __name__ == "__main__":
             os.system("chmod 600 /tmp/passport.tmp")
             os.system("echo '"+
                     password_database[account_name]+"' > /tmp/passport.tmp")
-            # launch vi to edit the entry
+            # launch less to display the entry
             # but lock the screen session after 30 seconds
             os.system("screen -c "+
                     sys.path[-1]+"/passport/screenrc less /tmp/passport.tmp")
+        logging.info("Display closed by user, exiting")
     elif args.choice == "remove":
         account_name = args.account
         database_name = args.database
         # decrypt password database
+        logging.info("User selected 'remove' subcommand")
+        logging.info("Reading and decrypting password database '"+
+                args.database+"'...")
         password_database, database_password = read_and_decrypt(database_name)
+        logging.info("Read and decrypted '"+args.database+"' successfully")
         # check to make sure the account exists in the database
         if account_name not in password_database.keys():
+            logging.info("No account under the name '"+
+                    account_name+"', notifying user and exiting")
             print("'"+account_name+"' does not exist in '"+database_name+"'")
         # otherwise, proceed with confirmation of deletion
         else:
             # confirm that the user really wants to delete the account
+            logging.info("Account found under name '"+
+                    account_name+"', confirming deletion with user...")
             response = ""
             while response not in ["y","Y","n","N"]:
                 response = input("Are you sure you want to delete '"+
                         account_name+"'? (y/n) ")
             if response in ["y","Y"]:
+                logging.info("Deletion of '"+
+                        account_name+"' confirmed, deleting...")
                 # delete the account
                 del password_database[account_name]
                 # encrypt and write new password database to disk
+                logging.info("Deletion complete, encrypting and writing...")
                 encrypt_and_write(password_database,
                         database_name,
                         database_password=database_password)
                 print("Removed account '"+account_name+"'")
+                logging.info("Encrypting and writing completed successfully"+
+                        ", exiting")
             else:
+                logging.info("Deletion of '"+account_name+"' aborted, exiting")
                 print("Aborted")
     elif args.choice == "changepw":
         # decrypt password database
+        logging.info("User selected 'changepw' subcommand")
+        logging.info("Reading and decrypting password database '"+
+                args.database+"'...")
         password_database, database_password = read_and_decrypt(args.database)
+        logging.info("Read and decrypted '"+args.database+"' successfully")
         # encrypt the database with a new password and write it to disk
+        logging.info("Prompting user for new password before encrypting and"+
+                " writing...")
         encrypt_and_write(password_database, args.database)
+        logging.info("Password changed successfully")
         print("Database password updated successfully")
